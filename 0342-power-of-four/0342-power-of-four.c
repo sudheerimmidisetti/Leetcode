f17b6ccc3f07c09b70d3a73cffb5fbb78a9d0db2
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-08-15 09:05:30 +0530

    Time: 0 ms (100%), Space: 8 MB (5.26%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.c
+++ 0342-power-of-four/0342-power-of-four.c
@@ -1,11 +1,11 @@
 bool isPowerOfFour(int n) {
-    int x=0,c=4;
-    while(n>=pow(c,x)) {
-        if(n==pow(c,x))     return true;
-        else {
-            c=4;
+    int x = 0, c = 4;
+    while (n >= pow(c, x)) {
+        if (n == pow(c, x))
+            return true;
+        else
             x++;
-        }
     }
+
     return false;
 }
