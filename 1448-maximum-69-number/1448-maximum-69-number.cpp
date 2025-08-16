dc10b6917b203236cff084db94bb3b12dc51218b
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-08-16 11:01:33 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.82%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -3,13 +3,13 @@ public:
     int maximum69Number(int num) {
         string s = to_string(num);
 
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '6') {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6') {
                 s[i] = '9';
                 break;
             }
         }
-        
+
         return stoi(s);
     }
 };
