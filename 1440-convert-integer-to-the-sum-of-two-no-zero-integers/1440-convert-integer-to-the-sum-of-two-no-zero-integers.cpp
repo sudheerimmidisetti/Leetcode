bd7095a0d98b16ff6eec5c694ad5d493cd1de64c
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-09-08 10:20:45 +0530

    Time: 0 ms (100%), Space: 9.2 MB (15.58%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -6,9 +6,9 @@ public:
 
         while (n) {
             string s = to_string(n);
-            string s2 = to_string(val);
+            s += to_string(val);
 
-            if (count(s.begin(), s.end(), '0') == 0 && count(s2.begin(), s2.end(), '0') == 0)
+            if (count(s.begin(), s.end(), '0') == 0)
                 return {n, val};
 
             n--;
