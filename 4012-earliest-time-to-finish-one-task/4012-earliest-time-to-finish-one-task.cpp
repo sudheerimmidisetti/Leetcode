9a3042f6cbd8659f35ec564d411771148eb2c318
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-09-14 16:05:56 +0530

    Time: 0 ms (100%), Space: 36.9 MB (75%) - LeetHub

---
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+
+        int minTime = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            minTime = min(minTime, tasks[i][0] + tasks[i][1]);
+        }
+
+        return minTime;
+    }
+};
