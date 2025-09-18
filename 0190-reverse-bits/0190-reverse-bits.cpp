809add6b7ec2c8426f156b2f2f8494b51f0aff18
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-09-18 15:13:39 +0530

    Time: 4 ms (29.63%), Space: 8.1 MB (92.21%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        unsigned int num = 0;
+
+        int i = 31;
+        while (n > 0 && i >= 0) {
+            num = num + ((n & 1) * pow(2, i));
+            n >>= 1;
+            i--;
+        }
+
+             return num;
+    }
+};
