b014f159032542ce0be0c5a8cbff985e2b271f0f
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2025-10-11 11:52:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (80.44%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        nums.push_back(0);
+        for (int i = n - 3; i >= 0; i--)
+            nums[i] += max(nums[i + 2], nums[i + 3]);
+
+        return max(nums[0], nums[1]);
+    }
+};
