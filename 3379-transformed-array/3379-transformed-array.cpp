d3031eddb3e2030440e43061e91b124cf15e696f
Immidisetti Sudheer <sudheerimmidisetti@gmail.com>
2026-02-05 18:02:38 +0530

    Time: 4 ms (79.67%), Space: 25.7 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
+
         vector<int> res(n);
         for (int i = 0; i < n; i++) {
             if (nums[i] > 0) {
@@ -11,11 +12,12 @@ public:
                 int newIndex = (i - abs(nums[i])) % n;
                 if (newIndex < 0)
                     newIndex += n;
+                
                 res[i] = nums[newIndex];
-            } else {
+            } else
                 res[i] = 0;
-            }
         }
+        
         return res;
     }
 };
