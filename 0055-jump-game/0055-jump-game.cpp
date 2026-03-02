705d0deb2d260c9636b596eccbaeb4c63e9a7d63
Immidisetti Sudheer <sudheerimmidisetti@gmail.com>
2026-03-02 10:26:03 +0530

    Time: 0 ms (100%), Space: 52.3 MB (51.72%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int idx = n - 1;
+        for (int i = n - 2; i >= 0; i--)
+            if (i + nums[i] >= idx)
+                idx = i;
+
+        return idx == 0;
+    }
+};
