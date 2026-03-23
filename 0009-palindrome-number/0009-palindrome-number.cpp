52e7dc8089b2af4abe8b435df472f1f4b088d5ee
Immidisetti Sudheer <sudheerimmidisetti@gmail.com>
2026-03-23 09:50:58 +0530

    Time: 3 ms (45.04%), Space: 10.9 MB (7.86%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        
+        string t = s;
+        reverse(t.begin(), t.end());
+
+        return s == t;
+    }
+};
