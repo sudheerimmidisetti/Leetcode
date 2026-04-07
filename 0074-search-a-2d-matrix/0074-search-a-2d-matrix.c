69f86b742bbab922296ec7cd8487493d95d85340
Immidisetti Sudheer <sudheerimmidisetti@gmail.com>
2026-04-07 18:39:33 +0530

    Time: 2 ms (0.84%), Space: 6.1 MB (100%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,11 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    for(int i=0; i<matrixSize; i++) {
+        for(int j=0; j< *matrixColSize; j++) {
+            if(matrix[i][j]==target) {
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+}
