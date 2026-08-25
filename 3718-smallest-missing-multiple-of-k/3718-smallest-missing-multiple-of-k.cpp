f4f99915c8b86fb7511a2b68a7fb8d133cb2fcfd
Immidisetti Sudheer <sudheerimmidisetti@gmail.com>
2026-08-25 19:51:21 +0530

    Time: 8 ms (9.55%), Space: 25.1 MB (45.06%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        set<int>st(nums.begin(), nums.end());
+
+        int minVal = INT_MAX;
+        for (int i = 1; i <= (k * 101); i++) {
+            if (st.find(k * i) == st.end()) {
+                minVal = k * i;
+                break;
+            }
+        }
+
+        return minVal;
+    }
+};
